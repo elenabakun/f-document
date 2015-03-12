747881b79b5a1c6aaa57c87e7166d8357a6339ce
floppox <floppox@users.noreply.github.com>
2015-03-12 18:04:15 +0200

    Update index.php

--- generator/index.php
+++ generator/index.php
@@ -1,5 +1,8 @@
 <?
 
+// Copyright © 2015 Elena Bakun Contacts: <floppox@gmail.com>
+// License: http://opensource.org/licenses/MIT
+
 function __autoload($classname) {
     include_once("./".strtolower($classname) .".class.php");
 }
