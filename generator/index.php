e7a63bb0ce6d68529bbaced573d63f6acc650101
floppox <floppox@users.noreply.github.com>
2015-03-12 16:10:04 +0200

    Create index.php

---
+++ generator/index.php
@@ -0,0 +1,38 @@
+<?
+
+function __autoload($classname) {
+    include_once("./".strtolower($classname) .".class.php");
+}
+
+$request = new Request();
+
+$document = new Document($request->template);
+
+$document->process();
+
+$matrix = new Compiler($document->search_filds(),$request->matrix);
+
+$document->replace_simple($matrix->simple); 
+
+if(!empty($matrix->groups_map)){
+	$document-> build_groups($matrix->group, $matrix->groups_map);
+	$matrix->expand_iteratives();
+	}
+
+$document->fill_iterative($matrix->iterative); 
+
+if($request->extention){
+	$action = 'extention';
+	$extention = $request->extention;
+	}
+elseif($request->action){
+	$action = $request->action;
+	$extention = false;
+	}
+else {
+	$action = 'show_link';
+	$extention = false;
+	}
+$document->close($action,$extention);
+
+?>
